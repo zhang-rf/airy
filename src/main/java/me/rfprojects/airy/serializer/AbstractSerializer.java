693b888da4f7df391250660564f1d67c19c77a5b
Taylor <610384825@qq.com>
2017-01-29 18:32:38 +0800

    Refactoring

--- src/main/java/me/rfprojects/airy/serializer/AbstractSerializer.java
+++ src/main/java/me/rfprojects/airy/serializer/AbstractSerializer.java
@@ -11,6 +11,8 @@ public abstract class AbstractSerializer implements Serializer {
 
     private ClassRegistry registry;
     private HandlerChain handlerChain;
+    private ThreadLocal<Boolean> sRecursionFlag = new ThreadLocal<>();
+    private ThreadLocal<Boolean> desRecursionFlag = new ThreadLocal<>();
 
     public AbstractSerializer(ClassRegistry registry, HandlerChain handlerChain) {
         this.registry = Objects.requireNonNull(registry);
@@ -23,6 +25,11 @@ public abstract class AbstractSerializer implements Serializer {
 
     @Override
     public void serialize(NioBuffer buffer, Object object, boolean writeClass, Class<?> reference, Type... generics) {
+        if (sRecursionFlag.get() != Boolean.TRUE) {
+            serialize(buffer, object, writeClass);
+            return;
+        }
+
         if (handlerChain.supportsType(reference))
             handlerChain.write(buffer, object, reference, generics);
         else {
@@ -39,25 +46,47 @@ public abstract class AbstractSerializer implements Serializer {
                     registry.writeClass(buffer, type != reference ? type : null);
                 writeObject(buffer, object, reference, generics);
             }
-            if (!writeClass && reference == null)
+        }
+    }
+
+    protected void serialize(NioBuffer buffer, Object object, boolean writeClass) {
+        sRecursionFlag.set(Boolean.TRUE);
+        try {
+            if (!writeClass)
                 registry.writeClass(buffer, null);
+            serialize(buffer, object, writeClass, null);
+        } finally {
+            sRecursionFlag.remove();
         }
     }
 
     @Override
     public Object deserialize(NioBuffer buffer, Class<?> type, Class<?> reference, Type... generics) {
+        if (desRecursionFlag.get() != Boolean.TRUE)
+            return deserialize(buffer, type);
+
         if (handlerChain.supportsType(reference))
             return handlerChain.read(buffer, reference, generics);
         else {
-            type = registry.readClass(buffer, type == null ? reference : type);
             if (type == null)
-                throw new UnknownClassException();
-            else if (type == Object.class)
+                type = registry.readClass(buffer, reference);
+            if (type == Object.class)
                 return new Object();
             else if (handlerChain.supportsType(type))
                 return handlerChain.read(buffer, reference, generics);
             else
                 return readObject(buffer, type, reference, generics);
+        }
+    }
+
+    protected Object deserialize(NioBuffer buffer, Class<?> type) {
+        desRecursionFlag.set(Boolean.TRUE);
+        try {
+            if ((type = registry.readClass(buffer, type)) == null)
+                throw new UnknownClassException();
+            return deserialize(buffer, type, null);
+        } finally {
+            desRecursionFlag.remove();
         }
     }
 }
--- src/main/java/me/rfprojects/airy/serializer/ReferencedSerializer.java
+++ src/main/java/me/rfprojects/airy/serializer/ReferencedSerializer.java
@@ -1,5 +1,8 @@
 package me.rfprojects.airy.serializer;
 
+import me.rfprojects.airy.core.ClassRegistry;
+import me.rfprojects.airy.core.NioBuffer;
+import me.rfprojects.airy.handler.chain.HandlerChain;
 import me.rfprojects.airy.util.ThreadLocalReference;
 
 import java.lang.ref.Reference;
@@ -11,6 +14,28 @@ public abstract class ReferencedSerializer extends AbstractSerializer {
 
     private ThreadLocalReference<Map<Object, Integer>> objectMapReference = new IdentityHashMapTLSR<>();
     private ThreadLocalReference<Map<Integer, Object>> addressMapReference = new IdentityHashMapTLSR<>();
+
+    public ReferencedSerializer(ClassRegistry registry, HandlerChain handlerChain) {
+        super(registry, handlerChain);
+    }
+
+    @Override
+    protected void serialize(NioBuffer buffer, Object object, boolean writeClass) {
+        try {
+            super.serialize(buffer, object, writeClass);
+        } finally {
+            objectMap().clear();
+        }
+    }
+
+    @Override
+    protected Object deserialize(NioBuffer buffer, Class<?> type) {
+        try {
+            return super.deserialize(buffer, type);
+        } finally {
+            addressMap().clear();
+        }
+    }
 
     protected Map<Object, Integer> objectMap() {
         return objectMapReference.get();
